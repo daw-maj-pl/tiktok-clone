8739bb952120070c4dc8db1d7cb7d00f986a4165
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-08-05 09:34:33 +0200

    Create Sidebar component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
+import Sidebar from '../components/Sidebar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -16,7 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       Navbar
       <div className="flex gap-6 md:gap-20 ">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          Sidebar
+          <Sidebar />
         </div>
         <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
           <Component {...pageProps} />
