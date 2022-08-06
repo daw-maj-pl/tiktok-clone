f58ff96583a271aa962b7a9a54d077ef5c891367
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-08-06 16:19:43 +0200

    Create footer lists

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -36,3 +36,28 @@ export const topics = [
     icon: <FaMedal />
   }
 ];
+
+export const footerList1 = [
+  'About',
+  'Newsroom',
+  'Store',
+  'Contact',
+  'Carrers',
+  'ByteDance',
+  'Creator Directory'
+];
+export const footerList2 = [
+  'TikTik for Good',
+  'Advertise',
+  'Developers',
+  'Transparency',
+  'TikTik Rewards'
+];
+export const footerList3 = [
+  'Help',
+  'Safety',
+  'Terms',
+  'Privacy',
+  'Creator Portal',
+  'Community Guidelines'
+];
