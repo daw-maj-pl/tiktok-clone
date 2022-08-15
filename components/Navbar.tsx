765def29e85bcba8d98e9808517b97ba838f2ba8
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-08-15 11:05:23 +0200

    Pass addUser function to createOrGetUser

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import Logo from '../utils/tiktik-logo.png';
 import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
-  const user = false;
+  const { userProfile, addUser } = useAuthStore();
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -23,11 +24,11 @@ const Navbar = () => {
       <div>SEARCH</div>
 
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div>{userProfile.userName}</div>
         ) : (
           <GoogleLogin
-            onSuccess={response => createOrGetUser(response)}
+            onSuccess={response => createOrGetUser(response, addUser)}
             onError={() => console.log('Login Failed')}
           />
         )}
