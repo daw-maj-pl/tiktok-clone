beeab98326bc430fe242358fcbedf0358eb6d7af
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-08-15 18:02:49 +0200

    Add hostname to next config file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['media.graphassets.com']
+    domains: ['media.graphassets.com', 'lh3.googleusercontent.com']
   }
 };
 
