3c4e9fceec20551c8ba6021c6534b14f4607b1cb
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-08-23 18:41:00 +0200

    Create Detail component

---
+++ pages/detail/[id].tsx
@@ -0,0 +1,76 @@
+import { useRef, useState } from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import axios from 'axios';
+import { BASE_URL } from '../../utils';
+import { Video } from '../../types';
+
+interface IProps {
+  postDetails: Video;
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoClick = () => {
+    if (isPlaying) {
+      videoRef?.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <>
+      {post && (
+        <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+          <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
+            <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+              <p>
+                <MdOutlineCancel className="text-white text-[35px]" />
+              </p>
+            </div>
+            <div className="relative">
+              <div className="lg:h-[100vh] h-[60vh]">
+                <video
+                  ref={videoRef}
+                  onClick={onVideoClick}
+                  loop
+                  src={post?.video?.asset.url}
+                  className="h-full cursor-pointer"
+                ></video>
+              </div>
+
+              <div className="absolute top-[45%] left-[45%] cursor-pointer">
+                {!isPlaying && (
+                  <button onClick={onVideoClick}>
+                    <BsFillPlayFill className="text-white text-6xl lg:text-8xl" />
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export const getServerSideProps = async ({
+  params: { id }
+}: {
+  params: { id: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: res.data }
+  };
+};
+
+export default Detail;
