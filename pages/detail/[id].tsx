2af03a829fcc437cc42c1ec06b8a4beca1468556
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-08-24 11:33:14 +0200

    Add functionality to navigate back after clicking cancel button

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 import { MdOutlineCancel } from 'react-icons/md';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
@@ -16,6 +17,7 @@ const Detail = ({ postDetails }: IProps) => {
   const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false);
 
   const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
 
   const onVideoClick = () => {
     if (isPlaying) {
@@ -39,7 +41,7 @@ const Detail = ({ postDetails }: IProps) => {
         <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
           <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
             <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
-              <p>
+              <p className="cursor-pointer" onClick={() => router.back()}>
                 <MdOutlineCancel className="text-white text-[35px]" />
               </p>
             </div>
