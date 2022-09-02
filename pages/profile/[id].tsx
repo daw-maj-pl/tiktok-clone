c4c33d1b6d3f2a958f1a1b03abd643a799080e58
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-09-02 11:38:55 +0200

    Create Videos and Liked buttons

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,7 +1,7 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import { GoVerified } from 'react-icons/go';
 import axios from 'axios';
-
 import { IUser, Video } from '../../types';
 import { BASE_URL } from '../../utils';
 
@@ -14,7 +14,11 @@ interface IProps {
 }
 
 const Profile = ({ data }: IProps) => {
+  const [showUserVideos, setShowUserVideos] = useState<Boolean>(true);
+
   const { user, userVideos, userLikedVideos } = data;
+  const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+  const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
 
   return (
     <div className="w-full">
@@ -37,6 +41,22 @@ const Profile = ({ data }: IProps) => {
           </p>
           <p className="capitalize md:text-xl text-gray-400 text-xs">
             {user.userName}
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+          <p
+            className={`text-xl font-semibold cursor-pointer ${videos} mt-2`}
+            onClick={() => setShowUserVideos(true)}
+          >
+            Videos
+          </p>
+          <p
+            className={`text-xl font-semibold cursor-pointer ${liked} mt-2`}
+            onClick={() => setShowUserVideos(false)}
+          >
+            Liked
           </p>
         </div>
       </div>
