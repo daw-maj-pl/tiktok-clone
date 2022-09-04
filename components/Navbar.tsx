cac8e025a570cb58237602c822a850ca07f2e95f
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-09-04 10:10:22 +0200

    Implement search form

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { IoMdAdd } from 'react-icons/io';
 import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
 import Logo from '../utils/tiktik-logo.png';
 import { createOrGetUser } from '../utils';
 import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState<string>('');
+  const router = useRouter();
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -23,7 +36,26 @@ const Navbar = () => {
         </div>
       </Link>
 
-      <div>SEARCH</div>
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={e => setSearchValue(e.target.value)}
+            className="bg-primary p-3 md:text-base font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0"
+            placeholder="Search accounts and videos"
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
 
       <div>
         {userProfile ? (
